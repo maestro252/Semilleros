0f9b0d5f30504771a149ba004443bfa24178eeae
Jonathan Eidelman <eidelmanjonathan@hotmail.com>
2014-09-09 22:01:49 -0500

    Mejorando looking glass

--- programitas/sistemasNumericos.cpp
+++ programitas/sistemasNumericos.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <vector>
-#include <math.h>
 using namespace std;
 
 vector <int> bases (int n, int base){
@@ -16,46 +13,21 @@ vector <int> bases (int n, int base){
 	numero.push_back(naux/base);
 	return numero;
 }
-vector <int> pasar(int n){ //el numero debe ser menor a 1024 aun no funciona
-	vector<int> numero(11,0);
-	bool sale = false;
-	for (int i = 0; i < numero.size() and !sale; ++i)
-	{
-		if(n < pow(2, i)){
-			numero[i - 1] = 1;
-			n -= pow(2,i - 1);
-			if(n <= 0) sale = true;
-		}else if(n == pow(2, i)){
-			sale = true;
-			numero[i] = 1;
-		}
-	}
-	reverse(numero.begin(), numero.end());
-	
-	for (int i = 0; i < numero.size(); ++i)
-	{
-		cout << numero[i];
-	}
-	cout << endl;
-	return numero;
-}
 
+bool comparar(vector<int> a){
+	for (int i = 0; i < (a.size()/2); ++i)
+	{
+		if(a[i] != a[(a.size() - i - 1)]) return false;
+	}
+	return true;
+}
 int main () {
 	int n;
 	while(cin >> n) {
-		int base = 0;
 		for (int i=2; i < n; ++i) {
-			base = i;
 			vector <int> numero = bases(n,i);
-			vector <int> palindromo = numero;
-			reverse(palindromo.begin(), palindromo.end());
-			bool iguales=true;
-			if(numero!=palindromo)iguales=false;
-			if(iguales) break;
+			if(comparar(numero)){ cout << i << endl; break; }
 		}
-		cout << n << " en base 2 es: ";
-		vector <int> numerito = pasar(n);
-		cout << base << endl;
 	}
 	return 0;
 }
